2136e3cef515059252eb88e69b19b8895d8817db
kimyounghoon <hungame@naver.com>
2022-07-13 00:00:20 +0900

    commit #2
    1) 터렛 수정본

--- Turret/Assets/Scripts/Bullet/Bullet.cs
+++ Turret/Assets/Scripts/Bullet/Bullet.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float Speed = 0.01f;
-    
+    public GameObject Player;
+
     private void Start()
     {
         Destroy(gameObject , 3f);
+        Player = GameObject.Find("Player");
     }
 
     private void Update()
     {
+        transform.LookAt(Player.transform);
         transform.Translate(0f, 0f, Speed);
         //transform.position = new Vector3(0f, 0f, Speed);
     }
