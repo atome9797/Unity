c6bcadfe6548f9a52c6992e116322297ba51beb8
kimyounghoon <hungame@naver.com>
2022-07-15 06:16:50 +0900

    220715 commit #1
    1) 유니런 업데이트

--- Unity-Programming-Essence-2021-main/Unity-Programming-Essence-2021-main/11/Uni-Run/Assets/Scripts/ScrollingObject.cs
+++ Unity-Programming-Essence-2021-main/Unity-Programming-Essence-2021-main/11/Uni-Run/Assets/Scripts/ScrollingObject.cs
@@ -5,8 +5,12 @@ public class ScrollingObject : MonoBehaviour {
     public float speed = 10f; // 이동 속도
 
     private void Update() {
-        // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
-        // 초당 speed의 속도로 왼쪽으로 평행이동
-        transform.Translate(Vector3.left * speed * Time.deltaTime); //(-1,0,0)
+
+        if(!GameManager.instance.isGameover)
+        {
+            // 게임 오브젝트를 왼쪽으로 일정 속도로 평행 이동하는 처리
+            // 초당 speed의 속도로 왼쪽으로 평행이동
+            transform.Translate(Vector3.left * speed * Time.deltaTime); //(-1,0,0)
+        }
     }
 }
