8577bcfc3b8c0725d78bb536e9bbb7bcd0f57d01
kimyounghoon <hungame@naver.com>
2022-07-20 23:38:18 +0900

    220720 commit #1
    1) 좀비 서바이벌

---
+++ Zombie/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+// 플레이어 캐릭터를 조작하기 위한 사용자 입력을 감지
+// 감지된 입력값을 다른 컴포넌트들이 사용할 수 있도록 제공
+public class PlayerInput : MonoBehaviour 
+{
+    public string MoveAxisName = "Vertical"; 
+    public string RotateAxisName = "Horizontal";
+    public string FireButtonName = "Fire1"; 
+    public string ReloadButtonName = "Reload";
+
+    // 값 할당은 내부에서만 가능
+    public float MoveDirection { get; private set; } 
+    public float RotateDirection { get; private set; }
+    public bool Fire { get; private set; } 
+    public bool Reload { get; private set; } 
+
+    // 매프레임 사용자 입력을 감지
+    private void Update() {
+        // 게임오버 상태에서는 사용자 입력을 감지하지 않는다
+        if (GameManager.Instance.isGameover)
+        {
+            MoveDirection = 0;
+            RotateDirection = 0;
+            Fire = false;
+            Reload = false;
+            return;
+        }
+
+        // move에 관한 입력 감지
+        MoveDirection = Input.GetAxis(MoveAxisName);
+        // rotate에 관한 입력 감지
+        RotateDirection = Input.GetAxis(RotateAxisName);
+        // fire에 관한 입력 감지
+        Fire = Input.GetButton(FireButtonName);
+        // reload에 관한 입력 감지
+        Reload = Input.GetButtonDown(ReloadButtonName);
+    }
+}
---
+++ Zombie/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+// 플레이어 캐릭터를 사용자 입력에 따라 움직이는 스크립트
+public class PlayerMovement : MonoBehaviour {
+    
+    [SerializeField]
+    private float MoveSpeed = 5f; // 앞뒤 움직임의 속도
+    public float RotateSpeed = 180f; // 좌우 회전 속도
+
+
+    private PlayerInput _input; // 플레이어 입력을 알려주는 컴포넌트
+    private Rigidbody _rigidbody; // 플레이어 캐릭터의 리지드바디
+    private Animator _animator; // 플레이어 캐릭터의 애니메이터
+
+    private static class AnimID
+    {
+        public static readonly int MOVE = Animator.StringToHash("Move");
+    }
+
+    private void Awake()
+    {
+        _input = GetComponent<PlayerInput>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();   
+    }
+
+    // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
+    private void FixedUpdate() {
+        // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
+        move();
+        rotate();
+
+        _animator.SetFloat(AnimID.MOVE , _input.MoveDirection);
+    }
+
+    // 입력값에 따라 캐릭터를 앞뒤로 움직임
+    private void move()
+    {                   //속력(MoveSpeed) * 시간(deltaTime)  => 캐릭터 기준으로 앞 혹은 뒤를 이동 
+        //거리 = 속력 * 시간
+        float movementAmount = MoveSpeed * Time.fixedDeltaTime;
+        //방향은 캐릭터 기준이다.
+        Vector3 direction = _input.MoveDirection * transform.forward;
+        Vector3 offset = movementAmount * direction; //벡터 연산이 적도록 곱셈을 작성한다.
+
+        _rigidbody.MovePosition(_rigidbody.position + offset);
+    }
+
+    // 입력값에 따라 캐릭터를 좌우로 회전
+    private void rotate() {
+
+        float rotationAmount = _input.RotateDirection * RotateSpeed * Time.fixedDeltaTime;
+        Quaternion deltaRotation = Quaternion.Euler(0f, rotationAmount, 0f);
+        _rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
+    }
+}
