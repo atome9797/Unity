235d00ec8826b851b3eac11e8819796aee013dac
ProgrammerPenguin <65073631+atome9797@users.noreply.github.com>
2022-08-08 13:12:35 +0900

    메뉴에서 캐릭터 선택시 이벤트 처리
    1. 메뉴에서 버튼 클릭시 캐릭터 변경되고 이벤트 실행됨

---
+++ UI_SelectCharacter/Assets/UIManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject[] characters;
+
+    private int SeletedID { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //characters[0] = Resources.Load<GameObject>("");
+    }
+
+    public void ChangeCharacter(int index)
+    {
+        if(index >= characters.Length)
+        {
+            return;
+        }
+
+        foreach(GameObject character in characters)
+
+        {
+            character.SetActive(false);
+
+            SeletedID = index;
+            characters[SeletedID].SetActive(true);
+        }
+    }
+
+    public void SceneChage()
+    {
+        
+    }
+}
